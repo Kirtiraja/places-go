38ede20744b941712bdf4960b6338bae3b7199d1
Student <example@example.com>
2019-11-04 15:33:41 -0800

    add clickable city destination to display object information

---
+++ README.md
@@ -0,0 +1,27 @@
+# Oh The Places You'll Go
+#### Places Web Application for Epicodus, 11/01/19
+#### By Celeste Renee Morton & Kirtiraja Zakheim
+## Description
+This website is an application where you can keep track of all the places you've been. Each destination should be an object with multiple properties, like location, landmarks, time of year, notes, etc. Display those properties when a user clicks on a place's name. Complete the business logic for your place object first, before tackling the user interface. Additionally, make sure to include specifications with example input and output in your project's README.
+## Specifications
+| Behavior: | Input Example: | Output Example: |
+| - | - | - |
+| **Our program should handle:** | **When it receives:** | **It should return:** |
+| when user clicks on city name it will display the associated properties for that city.| Austin| City,State,Season,Food,Activity|
+
+
+
+## Setup/Installation Requirements
+* Clone this repository
+* Open in your preferred editor
+* Control click to open with live server if using VS
+## Support and contact details
+Please contact me directly with any questions or concerns. You can reach me at 541-555-5555
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* jQuery
+### Legal
+### Copyright (c) 2019
+This software is installed under MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -10,18 +10,49 @@ DestinationList.prototype.addLocation = function(location) {
 
 // Business logic for Towns //
 
-function Location(state, city, season) {
+function Location(state, city, season, food, activity) {
   this.state = state,
   this.city = city,
-  this.season = season
+  this.season = season,
+  this.food = food,
+  this.activity = activity
+
 }
 
 Location.prototype.locationInfo = function() {
-  return this.state + " " + this.city;
+  return this.state + " " + this.season + " " + this.food + " " + this.activity;
 }
 
 var destinationList = new DestinationList();
-var location1 = new Location("Texas", "Austin", "Fall");
-var location2 = new Location("California", "Leucadia", "Winter");
-destinationList.addLocation(location);
-destinationList.addLocation(location2);
+
+var austin = new Location("Texas", "Austin", "Fall", ["seafood", "tacos"], ["cycling", "bat-viewing"]);
+
+var leucadia = new Location("California", "Leucadia", "Winter", ["burritos", "vegan"],"surfing", "SUP");
+
+destinationList.addLocation(boulder);
+destinationList.addLocation(austin);
+destinationList.addLocation(leucadia);
+var boulder = new Location("Colorado",  "Boulder", "Summer",["italian", "burmese"], ["trail-running", "skiing"]);
+
+// Front End Logic Goes Here:
+
+$(document).ready(function() {
+  console.log("Click1");
+  $("#austin").click(function(event) {
+    var output = austin.locationInfo();
+    $("#austinShow").text(output);
+
+    });
+
+    console.log("Click2");
+    $("#leucadia").click(function(event) {
+      var output = leucadia.locationInfo();
+      $("#leucadiaShow").text(output);
+
+});
+console.log("Click3");
+$("#boulder").click(function(event) {
+  var output = boulder.locationInfo();
+  $("#boulderShow").text(output);
+});
+});
